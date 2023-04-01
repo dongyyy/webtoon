e65ee029e12ccef8067d730b19376daf317edea5
ethan <noahkim.j@gmail.com>
2023-04-01 14:54:56 +0900

    FutureBuilder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,49 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key? key}) : super(key: key);
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
-
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 2,
-        centerTitle: true,
-        foregroundColor: Colors.green,
         backgroundColor: Colors.white,
-        title: const Text(
-          "오늘의 웝툰",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
+        appBar: AppBar(
+          elevation: 2,
+          centerTitle: true,
+          foregroundColor: Colors.green,
+          backgroundColor: Colors.white,
+          title: const Text(
+            "오늘의 웝툰",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: webtoons, //await 해줄 필요 없이 futureBuilder가 해줌
+          builder: (context, snapshot) {
+            //snapshot을 이용하면 Future의 상태를 알 수 있음 - Future가 데이터를 받았는지, 아니면 오류를 받았는지 알 수 있음. connectionState도 알 수 있음
+            if (snapshot.hasData) {
+              return const Text("There is data!");
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ));
   }
 }
