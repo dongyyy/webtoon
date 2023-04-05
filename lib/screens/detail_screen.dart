7a1aa915417a97cd983507221a81f57f5d5a6725
ethan <noahkim.j@gmail.com>
2023-04-05 23:05:14 +0900

    Futures

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
 
   const DetailScreen({
@@ -9,6 +12,21 @@ class DetailScreen extends StatelessWidget {
     required this.thumb,
     required this.id,
   }) : super(key: key);
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodeByid(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +38,7 @@ class DetailScreen extends StatelessWidget {
         foregroundColor: Colors.green,
         backgroundColor: Colors.white,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w600,
@@ -38,11 +56,11 @@ class DetailScreen extends StatelessWidget {
               Column(
                 children: [
                   Hero(
-                    tag:id,
+                    tag: widget.id,
                     child: Container(
                       clipBehavior: Clip.hardEdge,
                       width: 250,
-                      child: Image.network(thumb),
+                      child: Image.network(widget.thumb),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(15),
                           boxShadow: [
@@ -57,7 +75,7 @@ class DetailScreen extends StatelessWidget {
                     height: 10,
                   ),
                   Text(
-                    title,
+                    widget.title,
                     style: const TextStyle(
                       color: Colors.black,
                       fontSize: 20,
